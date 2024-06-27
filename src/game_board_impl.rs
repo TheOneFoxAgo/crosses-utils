35b1244655d259390f5cf0ac2c042b6bc6228b53
TheOneFoxAgo <nikitad2005@gmail.com>
2024-06-27 23:26:38 +0300

    Fixed interface according to readme

--- src/game_board_impl.rs
+++ src/game_board_impl.rs
@@ -1,3 +1,4 @@
+use crate::cell_type::*;
 use crate::*;
 
 pub struct GameBoardImpl<'a, B: GameBoard + ?Sized> {
@@ -7,30 +8,30 @@ impl<B: GameBoard + ?Sized> GameBoardImpl<'_, B> {
     pub fn make_move(
         &mut self,
         index: BoardIndex<B>,
-        player: CellPlayer<BoardCell<B>>,
+        player: EntryPlayer<BoardCell<B>>,
     ) -> Result<(), GameCoreError> {
         let entry = self.board.entry(index);
         match entry.get_type() {
-            CellType::Empty => {
-                if entry.is_active(player) {
-                    entry.set_type(CellType::Cross);
-                    entry.set_player(player);
-                    entry.unset_anchor();
+            CellType::Empty(empty) => {
+                if empty.is_active(player) {
+                    entry.cross_out(player);
                     self.board.adjacent(index).into_iter().for_each(|i| {
                         let entry = self.board.entry(i);
                         match entry.get_type() {
-                            CellType::Empty => {
-                                entry.activate(player);
+                            CellType::Empty(mut empty) => {
+                                empty.activate(player);
                             }
-                            CellType::Cross => {
-                                entry.activate(player);
+                            CellType::Cross(mut cross) if (cross.get_player() != player) => {
+                                cross.activate(player);
                             }
-                            CellType::Filled => {
-                                if entry.get_player() == player {
-                                    if let CellState::Dead = entry.get_state() {
-                                        self.board.traverse(i, Activate::<B> { player });
-                                        self.board.entry(index).set_anchor();
-                                        self.board.entry(i).set_state(CellState::NearAnchor);
+                            CellType::Filled(filled) => {
+                                if filled.get_player() == player {
+                                    if !filled.is_alive() {
+                                        // self.board.revive(i, )
+                                        // self.board.traverse(i, Activate::<B> { player });
+                                        // self.board.entry(index).set_anchor();
+                                        // self.board.entry(i).set_state(CellState::NearAnchor);
+                                        todo!();
                                     }
                                 }
                             }
@@ -42,10 +43,10 @@ impl<B: GameBoard + ?Sized> GameBoardImpl<'_, B> {
                     Err(GameCoreError::OutOfReach)
                 }
             }
-            CellType::Cross => {
+            CellType::Cross(_cross) => {
                 todo!()
             }
-            CellType::Filled => todo!(),
+            CellType::Filled(_filled) => todo!(),
             CellType::Border => Err(GameCoreError::BorderHit),
         }
     }
@@ -54,32 +55,5 @@ impl<B: GameBoard + ?Sized> GameBoardImpl<'_, B> {
     }
     pub fn init(&self) {
         unimplemented!();
-    }
-}
-
-struct Activate<B: GameBoard + ?Sized> {
-    player: CellPlayer<BoardCell<B>>,
-}
-impl<B: GameBoard + ?Sized> detail::Sealed for Activate<B> {}
-impl<B: GameBoard + ?Sized> Strategy for Activate<B> {
-    type Board = B;
-
-    fn is_traversed(&self, cell: &BoardCell<Self::Board>) -> bool {
-        cell.get_state() == CellState::Alive
-    }
-
-    fn process(
-        &self,
-        board: &mut Self::Board,
-        index: BoardIndex<Self::Board>,
-    ) -> core::ops::ControlFlow<()> {
-        let entry = board.entry(index);
-        match entry.get_type() {
-            CellType::Empty => entry.activate(self.player),
-            CellType::Cross if entry.get_player() != self.player => entry.activate(self.player),
-            CellType::Filled => entry.set_state(CellState::Alive),
-            _ => {}
-        }
-        core::ops::ControlFlow::Continue(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,17 @@ pub trait GameBoard {
     type Entry: BoardEntry;
     fn entry(&mut self, index: Self::Index) -> &mut Self::Entry;
     fn adjacent(&self, index: Self::Index) -> [Self::Index; 8];
-    fn traverse<S: Strategy>(&mut self, idx: Self::Index, strategy: S) -> Option<Self::Index>;
+    fn revive(&mut self, index: Self::Index, revive: impl Fn(EntryType<Self::Entry>));
+    fn kill(&mut self, index: Self::Index, kill: impl Fn(EntryType<Self::Entry>));
+    fn search(
+        &mut self,
+        index: Self::Index,
+        search: impl Fn(EntryType<Self::Entry>) -> core::ops::ControlFlow<Self::Index>,
+    ) -> Option<Self::Index>;
     fn make_move(
         &mut self,
         index: Self::Index,
-        player: CellPlayer<Self::Entry>,
+        player: EntryPlayer<Self::Entry>,
     ) -> Result<(), GameCoreError> {
         GameBoardImpl { board: self }.make_move(index, player)
     }
@@ -23,51 +29,62 @@ pub trait GameBoard {
 }
 type BoardIndex<B> = <B as GameBoard>::Index;
 type BoardCell<B> = <B as GameBoard>::Entry;
+type EntryType<E> = CellType<
+    <E as BoardEntry>::Empty,
+    <E as BoardEntry>::Cross,
+    <E as BoardEntry>::Filled,
+    <E as BoardEntry>::Player,
+>;
+type EntryPlayer<E> = <E as BoardEntry>::Player;
 
 pub trait BoardEntry {
     type Player: Copy + PartialEq;
-    fn get_type(&self) -> CellType;
-    fn set_type(&mut self, new_type: CellType);
-    fn get_player(&self) -> Self::Player;
-    fn set_player(&mut self, player: Self::Player);
-    fn is_active(&self, player: Self::Player) -> bool;
-    fn activate(&mut self, player: Self::Player);
-    fn deactivate(&mut self, player: Self::Player);
-    fn is_anchor(&self) -> bool;
-    fn set_anchor(&mut self);
-    fn unset_anchor(&mut self);
-    fn get_previous_player(&self) -> Self::Player;
-    fn set_previous_player(&mut self, player: Self::Player);
-    fn get_state(&self) -> CellState;
-    fn set_state(&mut self, new_state: CellState);
+    type Empty: cell_type::Empty<Player = Self::Player>;
+    type Cross: cell_type::Cross<Player = Self::Player>;
+    type Filled: cell_type::Filled<Player = Self::Player>;
+    fn get_type(&mut self) -> EntryType<Self>;
+    fn cross_out(&mut self, player: Self::Player) -> Self::Cross;
+    fn fill(&mut self, player: Self::Player) -> Self::Filled;
+    fn remove_fill(&mut self, player: Self::Player) -> Self::Cross;
+    fn remove_cross(&mut self, player: Self::Player) -> Self::Empty;
 }
-pub enum CellType {
-    Empty,
-    Cross,
-    Filled,
+
+pub enum CellType<E, C, F, P>
+where
+    E: cell_type::Empty<Player = P>,
+    C: cell_type::Cross<Player = P>,
+    F: cell_type::Filled<Player = P>,
+{
+    Empty(E),
+    Cross(C),
+    Filled(F),
     Border,
 }
-
-#[derive(PartialEq, Eq)]
-pub enum CellState {
-    Dead,
-    Alive,
-    NearAnchor,
-    Between,
-}
-type CellPlayer<C> = <C as BoardEntry>::Player;
-
-pub trait Strategy: detail::Sealed {
-    type Board: GameBoard + ?Sized;
-    fn is_traversed(&self, cell: &BoardCell<Self::Board>) -> bool;
-    fn process(
-        &self,
-        board: &mut Self::Board,
-        index: BoardIndex<Self::Board>,
-    ) -> core::ops::ControlFlow<()>;
-}
-mod detail {
-    pub trait Sealed {}
+pub mod cell_type {
+    pub trait Empty {
+        type Player;
+        fn is_active(&self, player: Self::Player) -> bool;
+        fn activate(&mut self, player: Self::Player);
+        fn deactivate(&mut self, player: Self::Player);
+    }
+    pub trait Cross {
+        type Player;
+        fn get_player(&self) -> Self::Player;
+        fn is_active(&self, player: Self::Player) -> bool;
+        fn activate(&mut self, player: Self::Player);
+        fn deactivate(&mut self, player: Self::Player);
+        fn is_anchor(&self) -> bool;
+        fn set_anchor(&mut self, is_anchor: bool);
+    }
+    pub trait Filled {
+        type Player;
+        fn get_player(&self) -> Self::Player;
+        fn get_previous_player(&self) -> Self::Player;
+        fn is_alive(&self) -> bool;
+        fn set_alive(&mut self, is_alive: bool);
+        fn is_important(&self) -> bool;
+        fn set_important(&mut self, is_important: bool);
+    }
 }
 
 pub enum GameCoreError {
