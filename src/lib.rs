40a114ff1ef5109f3b7e45c44cdf8a9b3e586519
TheOneFoxAgo <nikitad2005@gmail.com>
2024-07-02 16:59:46 +0300

    I give up. I'm using Cell

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,48 @@
-mod game_board_impl;
-use game_board_impl::GameBoardImpl;
-
-pub trait GameBoard: Sized {
+use core::cell::Cell;
+use core::ops::ControlFlow;
+pub trait Engine {
     type Index: Copy;
-    type Entry: BoardEntry;
-    fn entry(&mut self, index: Self::Index) -> Self::Entry;
+    type Data: Data;
+    fn cell(&self, index: Self::Index) -> &Cell<Self::Data>;
     fn adjacent(&self, index: Self::Index) -> [Self::Index; 8];
-    fn revive(&mut self, index: Self::Index, revive: impl Fn(CellType<Self::Entry>));
-    fn kill(&mut self, index: Self::Index, kill: impl Fn(CellType<Self::Entry>));
+    fn revive(&self, index: Self::Index, revive: impl Fn(Self::Index));
+    fn kill(&self, index: Self::Index, kill: impl Fn(Self::Index));
     fn search(
-        &mut self,
+        &self,
         index: Self::Index,
-        search: impl Fn(CellType<Self::Entry>) -> core::ops::ControlFlow<Self::Index>,
+        search: impl Fn(Self::Index) -> ControlFlow<Self::Index>,
     ) -> Option<Self::Index>;
     fn make_move(
         &mut self,
         index: Self::Index,
-        player: EntryPlayer<Self::Entry>,
-    ) -> Result<(), GameCoreError> {
-        GameBoardImpl { board: self }.make_move(index, player)
-    }
-    fn cancel_move(&mut self, index: Self::Index) -> Result<(), GameCoreError> {
-        GameBoardImpl { board: self }.cancel_move(index)
-    }
-    fn init(&mut self) {
-        GameBoardImpl { board: self }.init()
-    }
+        player: <Self::Data as Data>::Player,
+    ) -> Result<(), GameCoreError>;
+    fn cancel_move(&mut self, index: Self::Index) -> Result<(), GameCoreError>;
+    fn init(&mut self);
 }
-type BoardIndex<B> = <B as GameBoard>::Index;
-type BoardCell<B> = <B as GameBoard>::Entry;
-type EntryPlayer<E> = <E as BoardEntry>::Player;
 
-pub trait BoardEntry: Sized {
+pub trait Data: Copy {
     type Player: Copy + PartialEq;
-    type Empty: cell_type::Empty<Player = Self::Player>;
-    type Cross: cell_type::Cross<Player = Self::Player>;
-    type Filled: cell_type::Filled<Player = Self::Player>;
-    fn get_type(&mut self) -> CellType<Self>;
-    fn cross_out(&mut self, player: Self::Player) -> Self::Cross;
-    fn fill(&mut self, player: Self::Player) -> Self::Filled;
-    fn remove_fill(&mut self, player: Self::Player) -> Self::Cross;
-    fn remove_cross(&mut self, player: Self::Player) -> Self::Empty;
-}
+    fn kind(self) -> DataKind;
+    fn player(self) -> Self::Player;
+    fn previous_player(self) -> Self::Player;
+    fn is_active(self) -> bool;
+    fn set_active(self, new: bool) -> Self;
+    fn is_important(self) -> bool;
+    fn set_important(self, new: bool) -> Self;
+    fn is_alive(self) -> bool;
+    fn set_alive(self, new: bool) -> Self;
 
-pub enum CellType<E: BoardEntry> {
-    Empty(E::Empty),
-    Cross(E::Cross),
-    Filled(E::Filled),
-    Border,
+    fn cross_out(self, player: Self::Player) -> Self;
+    fn fill(self, player: Self::Player) -> Self;
+    fn remove_fill(self, player: Self::Player) -> Self;
+    fn remove_cross(self, player: Self::Player) -> Self;
 }
-pub mod cell_type {
-    pub trait Empty {
-        type Player;
-        fn is_active(&self, player: Self::Player) -> bool;
-        fn activate(&mut self, player: Self::Player);
-        fn deactivate(&mut self, player: Self::Player);
-    }
-    pub trait Cross {
-        type Player;
-        fn get_player(&self) -> Self::Player;
-        fn is_active(&self, player: Self::Player) -> bool;
-        fn activate(&mut self, player: Self::Player);
-        fn deactivate(&mut self, player: Self::Player);
-        fn is_anchor(&self) -> bool;
-        fn set_anchor(&mut self, is_anchor: bool);
-    }
-    pub trait Filled {
-        type Player;
-        fn get_player(&self) -> Self::Player;
-        fn get_previous_player(&self) -> Self::Player;
-        fn is_alive(&self) -> bool;
-        fn set_alive(&mut self, is_alive: bool);
-        fn is_important(&self) -> bool;
-        fn set_important(&mut self, is_important: bool);
-    }
+pub enum DataKind {
+    Empty,
+    Cross,
+    Filled,
+    Border,
 }
 
 pub enum GameCoreError {
